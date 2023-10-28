6f87f504a627645399d59276fbbb5764389297a7
Dilnae <dilnaerennan84@gmail.com>
2023-10-28 16:45:59 -0300

    FEAT: alteração no status http

--- src/controllers/ArtesaosControllers.js
+++ src/controllers/ArtesaosControllers.js
@@ -53,7 +53,7 @@ class ArtesaoController {
                     res.status(406).json({ message: erro.message })
 
                 } else if (erro.message == "senha inválida, minimo 6 caracteres"); {
-                    res.status(405).json({ message: erro.message })
+                    res.status(401).json({ message: erro.message })
                 }
                 
                 res.status(400).json({ message: erro.message })
